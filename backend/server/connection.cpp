b7f1fe36301b593e55262d7df328c6243d3feb86
Danny Piper <djpiper28@gmail.com>
2023-02-14 22:08:07 +0000

    feat: added context virtual class, this will be used to provide stream queues

--- backend/server/connection.cpp
+++ backend/server/connection.cpp
@@ -1,8 +1,49 @@
 #include "./connection.h"
+#include "../testing.h/logger.h"
+#include <stdexcept>
 
 using namespace Linkedin;
 
 Connection::Connection()
 {
     this->connection_time = time(NULL);
+    this->context = nullptr;
+}
+
+Connection::~Connection()
+{
+    if (this->context!= nullptr) {
+        delete this->context;
+    }
+}
+
+void Connection::set_context(ConnectionContext *context)
+{
+    this->context = context;
+}
+
+ConnectionContext *Connection::get_context()
+{
+    return this->context;
+}
+
+ConnectionContext::ConnectionContext()
+{
+
+}
+
+ConnectionContext::~ConnectionContext()
+{
+
+}
+
+bool ConnectionContext::has_message()
+{
+    return false;
+}
+
+std::string ConnectionContext::next_message()
+{
+    lprintf(LOG_ERROR, "Cannot get next_message for unspecified context\n");
+    throw std::runtime_error("Cannot get next_message for unspecified context\n");
 }
--- backend/server/connection.h
+++ backend/server/connection.h
@@ -1,15 +1,29 @@
 #pragma once
 #include <time.h>
+#include <string>
 
 namespace Linkedin
 {
+
+class ConnectionContext
+{
+public:
+    ConnectionContext();
+    virtual ~ConnectionContext();
+    virtual bool has_message();
+    virtual std::string next_message();
+};
 
 class Connection
 {
 public:
     Connection();
+    ~Connection();
+    void set_context(ConnectionContext *context);
+    ConnectionContext *get_context();
 private:
     time_t connection_time;
+    ConnectionContext *context;
 };
 
 }
